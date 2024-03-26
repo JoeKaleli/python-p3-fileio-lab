b1742176d9c42cad27c847b0b2ea9f9002596d7c
JoeKaleli <kalelimburu9@gmail.com>
2024-03-26 08:40:01 +0300

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,12 +1,22 @@
+
+
+
 def write_file(file_name, file_content):
     with open(f"{file_name}.txt","w") as file:
         file.write(file_content)
 
 def append_file(file_name, append_content):
     with open(f"{file_name}.txt", "a") as file:
-        file.write(f"\n{append_content}")
+        file.write(f"{append_content}")
 
 
 def read_file(file_name):
       with open(f"{file_name}.txt", "r") as file:
         return file.read().strip()
+
+
+# use write_file function. 
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+
